797ecb6319e9e921fea50a7b485adc811e5ad45c
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-14 06:04:42 +0600

    user created

--- script.ts
+++ script.ts
@@ -1,7 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-async function main() {}
+async function main() {
+  const user = await prisma.user.create({ data: { name: "Shourov Ahmed" } });
+  console.log(user);
+}
 
 main()
   .catch((e) => {
